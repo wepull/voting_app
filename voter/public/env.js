fb4b84ac3a0f0dc19b019b7ae62dae4215022947
apsgangwar <apoorvasinghgangwar@gmail.com>
2021-12-13 20:04:51 +0530

    minor change

--- voter/public/env.js
+++ voter/public/env.js
@@ -3,5 +3,5 @@
 
   // Environment variables
   window["env"]["ballotEndpoint"] = "roost-controlplane:30080";
-  window["env"]["ec_server_endpoint"] = "roost-controlplane:30081";
+  window["env"]["ecServerEndpoint"] = "roost-controlplane:30081";
 })(this);
--- voter/public/env.template.js
+++ voter/public/env.template.js
@@ -3,10 +3,7 @@
 
   // Environment variables
   var ballotEndpointEnvVar = `$REACT_APP_BALLOT_ENDPOINT`;
-  var ec_server_endpoint = `$REACT_APP_EC_SERVER_ENDPOINT`;
+  var ecServerEndpoint = `$REACT_APP_EC_SERVER_ENDPOINT`;
   window["env"]["ballotEndpoint"] = ballotEndpointEnvVar.replace(/['"]/gi, "");
-  window["env"]["ec_server_endpoint"] = ec_server_endpoint.replace(
-    /['"]/gi,
-    ""
-  );
+  window["env"]["ecServerEndpoint"] = ecServerEndpoint.replace(/['"]/gi, "");
 })(this);
