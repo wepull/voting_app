859d728c5678049441d1ba3c1a6b467a744b1e77
apsgangwar <apoorvasinghgangwar@gmail.com>
2021-12-13 20:01:56 +0530

    voter: add server endpoint

--- voter/public/env.template.js
+++ voter/public/env.template.js
@@ -3,5 +3,10 @@
 
   // Environment variables
   var ballotEndpointEnvVar = `$REACT_APP_BALLOT_ENDPOINT`;
+  var ec_server_endpoint = `$REACT_APP_EC_SERVER_ENDPOINT`;
   window["env"]["ballotEndpoint"] = ballotEndpointEnvVar.replace(/['"]/gi, "");
+  window["env"]["ec_server_endpoint"] = ec_server_endpoint.replace(
+    /['"]/gi,
+    ""
+  );
 })(this);
