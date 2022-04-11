fa6b34845d0c9e4ef794c77f0c06e0f27a4a47b5
Garvit-Jethwani <garvitjethwani@gmail.com>
2022-04-11 19:11:42 +0530

    ZBIO-3220 : Iterating over all candidates

--- service-test-suite/voter/cypress/integration/voter.spec.js
+++ service-test-suite/voter/cypress/integration/voter.spec.js
@@ -2,64 +2,49 @@ const { expect, assert } = require("chai")
 const { it } = require("mocha")
 
 describe('Checking Voter Webpage ', () => {
-  
+
   it('Visits voter webpage', () => {
-    cy.visit('http://default.voter.10.10.0.10.nip.io/') // Ingress Endpoint
 
-    cy.intercept(
-      {
-        method: 'POST', 
-        url: '*ballot*', 
+    cy.visit('http://default.voter.10.10.0.10.nip.io/')
+
+    // Iterating over all the candidates and voting for them 
+    cy.get('.cardContent').each((element,index,list)=>{
+
+      let candidateName = element.text()
+
+      if(candidateName){
+
+        cy.visit('http://default.voter.10.10.0.10.nip.io/') // Ingress Endpoint
+
+        cy.intercept(
+        {
+          method: 'POST', 
+          url: '*ballot*', 
+        }
+        ).as('postresult')
+
+        cy.contains(candidateName).click()
+        
+        cy.get('.selectedCard')
+        .should('be.visible')
+
+        cy.wait('@postresult',{ responseTimeout: 5000 }).then((interception) => {
+          assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
+        })
+
+        cy.contains('Show Results')
+        .click()
+  
+        cy.url()
+        .should('be.equal','http://default.voter.10.10.0.10.nip.io/voter/result')
+
+        cy.contains(candidateName)
+        cy.reload()
+
+      }else {
+        cy.log("Add candidates first !")
       }
-    ).as('postresult')
-    
-
-    cy.contains('Docker')
-    .should('be.visible')
-    .click()
-
-    cy.get('.selectedCard')
-    .should('be.visible')
-
-    cy.wait('@postresult',{ responseTimeout: 5000 }).then((interception) => {
-      assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
     })
-
-    cy.reload()
-
-    cy.contains("Roost")
-    .should('be.visible')
-    .click()
-
-    cy.get('.selectedCard')
-    .should('be.visible')
-
-    cy.wait('@postresult',{ responseTimeout: 5000 }).then((interception) => {
-      assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-    })
-
-    cy.reload()
-
-    cy.contains("Rancher")
-    .should('be.visible')
-    .click()
-
-    cy.get('.selectedCard')
-    .should('be.visible')
-
-    cy.wait('@postresult',{ responseTimeout: 5000 }).then((interception) => {
-      assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-    })
-
-    cy.contains('Show Results')
-    .click()
-
-    cy.url()
-    .should('be.equal','http://default.voter.10.10.0.10.nip.io/voter/result')
-
-    cy.contains('Roost').should('be.visible')
-    cy.contains('Docker').should('be.visible')
-    cy.contains('Rancher').should('be.visible')
   
   })
 
