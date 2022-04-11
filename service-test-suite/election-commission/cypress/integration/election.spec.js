870dfacf8c2c7c0d28f7c805c725ce1a524b5317
Garvit-Jethwani <garvitjethwani@gmail.com>
2022-04-11 12:31:24 +0530

    ZBIO-3220 : Add more cypress tests

--- service-test-suite/election-commission/cypress/integration/election.spec.js
+++ service-test-suite/election-commission/cypress/integration/election.spec.js
@@ -1,29 +1,73 @@
-// <reference types="cypress" />
+const { expect } = require("chai")
 
-context('Tesing Election Commission UI', () => {
+describe('Testing Election Commission Webpage', () => {
 
-  it('Testing valid text in web-age',()=>{
-    cy.visit('http://roost-controlplane:30031/ec')
+  it('Testing valid text in web-page',()=> {
+    cy.visit('http://default.ec.10.10.0.10.nip.io/')
+   
     cy.contains('Election Commission Admin Portal')
-    cy.contains("Add Candidate").click()
+    cy.contains("Add Candidate").should('be.visible').click()
     cy.contains('Add your candidates for election of K8s distribution here')
     cy.contains("Candidate's Name")
     cy.contains("Candidate's Picture")
   })
 
-  it('Testing the functionalities inside the web-page',()=>{
-    cy.visit('http://roost-controlplane:30031/ec')
+  it('Testing adding the candidate in web-page',()=>{
+    cy.visit('http://default.ec.10.10.0.10.nip.io/')
+
+    cy.intercept(
+      {
+        method: 'POST', 
+        url: '*ecserver*', 
+      }
+    ).as('postresult')  
+
     cy.contains("Add Candidate").click()
     cy.get("#candidateNameInput").type("RoostK8s")
     cy.get("#candidateImageUrlInput").type("https://content.roost.io/content/images/2021/05/Roost-3D-Metallic---Horizontal---Light-Silver.png")
-    cy.contains("Submit").click()
-    //cy.intercept('POST', 'http://10.10.0.10:30080',{statusCode: 201})
-    //cy.contains("Candidate added succesfully!")
+    cy.contains("Submit").should('be.visible').click()
+
+    cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
+
+      assert.isNotNull(interception.response.body, '{code: 201 }')
+    })
   })
+
+  it('Testing deleting the candidate in web-page',()=>{
+
+    cy.visit('http://default.ec.10.10.0.10.nip.io/')
+    cy.intercept(
+      {
+        method: 'DELETE',
+        url: '*ecserver*', 
+      }
+    ).as('deleteresult')   
+    cy.contains("Delete").click()
+    cy.wait('@deleteresult',{ responseTimeout: 35000 }).then((interception) => {
+
+      assert.isNotNull(interception.response.body, '{code: 201}')
+    })
+  })
+
+  it('Testing add/delete of candidates in backend',()=>{
+
+    let candidatesSize 
+    cy.request('http://default.ecserver.10.10.0.10.nip.io/').then((resp)=>{
+      candidatesSize = resp.body.Candidates.length
+    })
+
+    if(candidatesSize) {
+      cy.visit('http://default.ec.10.10.0.10.nip.io/')
+      cy.contains("Delete").click()
+      cy.request('http://default.ecserver.10.10.0.10.nip.io/').then((resp)=>{
+  
+        expect(resp.body.Candidates.length).to.equal(candidatesSize-1)
+      })
+    }
+    else {
+      cy.contains('No Candidates').should('be.visible')
+    }
+
+  })
+
 })
-// context('Testing voter UI',() => {
-//   it('Testing voter UI',()=>{
-//     cy.visit('http://roost-controlplane:30030')
-//     cy.contains('RoostK8s').click()
-//   })
-// })
--- service-test-suite/voter/cypress/integration/voter.spec.js
+++ service-test-suite/voter/cypress/integration/voter.spec.js
@@ -1,50 +1,77 @@
-// sample_spec.js.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
-describe('My First Test', () => {
+const { expect, assert } = require("chai")
+const { it } = require("mocha")
+
+describe('Checking Voter Webpage ', () => {
+
   it('Visits voter webpage', () => {
-    cy.visit(Cypress.env('ROOST_SVC_URL'))
+    cy.visit('http://default.voter.10.10.0.10.nip.io/') // Ingress Endpoint
 
-  cy.intercept(
-  {
-    method: 'POST', // Route all POST requests
-    url: '*ballot*', // that have a URL that matches '/users/*'
-  }
-).as('postresult')
+    cy.intercept(
+      {
+        method: 'POST', 
+        url: '*ballot*', 
+      }
+    ).as('postresult')
+    
 
-cy.contains("Docker").click()
+    cy.contains('Docker')
+    .should('be.visible')
+    .click()
 
-cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
+    cy.wait('@postresult',{ responseTimeout: 5000 }).then((interception) => {
+      assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
+    })
 
-  assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-})
+    cy.reload()
 
-// cy.reload()
-// cy.contains("MiniKube").click()
+    cy.contains("Roost")
+    .should('be.visible')
+    .click()
 
-// cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
-//   assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-// })
-cy.reload()
-cy.contains("Roost").click()
+    cy.wait('@postresult',{ responseTimeout: 5000 }).then((interception) => {
+      assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
+    })
 
-cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
-  assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-})
-cy.reload()
-cy.contains("Rancher").click()
+    cy.reload()
 
-cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
-  assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-})
+    cy.contains("Rancher")
+    .should('be.visible')
+    .click()
 
-cy.visit(Cypress.env('ROOST_SVC_HOST')+'/voter/result')
-cy.contains('Roost')
-cy.contains('Docker')
-//cy.contains('MiniKube')
-cy.contains('Rancher')
+    cy.wait('@postresult',{ responseTimeout: 5000 }).then((interception) => {
+      assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
+    })
+
+    cy.contains('Show Results')
+    .click()
+
+    cy.url()
+    .should('be.equal','http://default.voter.10.10.0.10.nip.io/voter/result')
+
+    cy.contains('Roost').should('be.visible')
+    cy.contains('Docker').should('be.visible')
+    cy.contains('Rancher').should('be.visible')
+  
   })
+
+  it('Check votes registered in backend ',() => {
+  
+    let voteCount 
+    cy.request('http://default.ballot.10.10.0.10.nip.io/').then((response)=> {
+      voteCount = response.body.total_votes 
+    })
+
+    cy.visit('http://default.voter.10.10.0.10.nip.io/') // Ingress Endpoint
+  
+    cy.contains("Roost")
+    .should('be.visible')
+    .click()
+
+
+    cy.request("http://default.ballot.10.10.0.10.nip.io/").then( (resp) => {
+      assert(expect(resp.body.total_votes).to.eq(voteCount+1))
+    })
+
+  })
+
 })
